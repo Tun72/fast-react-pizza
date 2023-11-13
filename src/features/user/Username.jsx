e407f66f3a942d49dadc623871972fdbf30d7f76
Tun72 <tunt72553@gmail.com>
2023-11-13 17:42:49 +0630

    first commiy

---
+++ src/features/user/Username.jsx
@@ -0,0 +1,9 @@
+function Username() {
+    return (
+        <div className="text-sm font-semibold">
+            Tun
+        </div>
+    )
+}
+
+export default Username
--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,11 +1,12 @@
 import { Link } from "react-router-dom"
 import SearchOrder from "../features/order/SearchOrder"
-
+import Username from "../features/user/Username"
 function Header() {
     return (
-        <header>
-            <Link to="/">Fast React Pizza co.</Link>
+        <header  className="bg-yellow-500 uppercase">
+            <Link to="/" className="tracking-widest">Fast React Pizza co.</Link>
             <SearchOrder />
+            <Username />
         </header>
     )
 }
